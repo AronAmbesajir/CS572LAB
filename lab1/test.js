f594019c95bf3234775f6a559a7329dc1925ec76
Aron <82886810+AronAmbesajir@users.noreply.github.com>
2021-05-19 17:50:07 -0500

    setTimeout added

--- lab1/test.js
+++ lab1/test.js
@@ -1,6 +1,3 @@
-const child_process = require("child_process");
 console.log("1: Start");
-const newProcess = child_process.spawn("node", ["fibonacci.js"],
-    { stdio: "inherit" });
-
-console.log("2: End");
+setTimeout(() => require("./Fibonacci"),1000);
+console.log("2: End"); 
