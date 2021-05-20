000ee346442b8a62973699d134167dc8c08c28b9
Aron <82886810+AronAmbesajir@users.noreply.github.com>
2021-05-19 20:48:56 -0500

    add image

--- lab2/app12.js
+++ lab2/app12.js
@@ -1,4 +1,3 @@
-
 const express = require("express");
 const app=express();
 const path=require("path");
@@ -15,15 +14,6 @@ console.log(req.method,req.url);
 next();
 });
 
-const express = require("express");
-const app=express();
-const path=require("path");
-app.set("port",5000);
-app.use("/public", express.static(path.join(__dirname,"public")));
-app.get("/",function(req,res){
-    console.log("Get received");
-    res.status(200).sendFile(path.join(__dirname,"public","index.html"));
-});
 
 const server=app.listen(app.get("port"),function(){
    const port = server.address().port;
